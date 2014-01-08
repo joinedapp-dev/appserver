348c815a02652b5015211b0f0def931bd3e15ca1
joinedapp <arash_hassibi@yahoo.com>
2014-01-08 03:07:00 +0000

    UPDATE: beginning to implement routes

--- server.js
+++ server.js
@@ -17,6 +17,54 @@ app.get('/', function(request, response) {
   response.send(data);
 });
 
+//res.setHeader({ 'Content-Type': 'application/json' });
+app.get('/:table', function(req,res){});
+
+/*
+app.get('/:table', function(req,res){
+    connectionpool.getConnection(function(err, connection) {
+        if (err) {
+            console.error('CONNECTION error: ',err);
+            res.statusCode = 503;
+            res.send({
+                result: 'error',
+                err:    err.code
+            });
+        } else {
+            connection.query('SELECT * FROM '+req.params.table+' ORDER BY id DESC LIMIT 20', req.params.id, function(err, rows, fields) {
+                if (err) {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.send({
+                        result: 'error',
+                        err:    err.code
+                    });
+                }
+                res.send({
+                    result: 'success',
+                    err:    '',
+                    fields: fields,
+                    json:   rows,
+                    length: rows.length
+                });
+                connection.release();
+            });
+        }
+    });
+});
+*/
+
+app.get('/:table/:id', function(req,res){});
+
+app.post('/:table', function(req,res){});
+
+app.put('/:table/:id', function(req,res){});
+
+app.delete('/:table/:id', function(req,res){});
+
+
+
+
 /*
 // Render example.com/orders
 app.get('/orders', function(request, response) {
