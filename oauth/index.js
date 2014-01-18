e5169366d4ba4d41abae969842d48da745bfd68f
joinedapp <arash_hassibi@yahoo.com>
2014-01-18 00:19:45 +0000

    UPDATE: cleanup code

--- oauth/index.js
+++ oauth/index.js
@@ -1,3 +1,10 @@
+// dependencies
+var passport         = require('passport')
+  , TwitterStrategy  = require('passport-twitter').Strategy
+  , FacebookStrategy = require('passport-facebook').Strategy
+  , GoogleStrategy   = require('passport-google').Strategy
+  , nosqldb          = require('../nosql_db');
+
 var ids = {
     facebook: {
 	clientID: process.env.FACEBOOK_CONSUMER_KEY,
@@ -15,4 +22,141 @@ var ids = {
     }
 }
 
-module.exports = ids
+var getId = function(oauth_method, profile){
+    if (oauth_method=='FACEBOOK'){
+	return profile.id;
+    }else if(oauth_method=='GOOGLE'){
+	return profile.identifier;
+    }else if(oauth_method=='TWITTER'){
+	return profile.id;
+    }else{
+	throw("Error: Unsupported OAuth service");
+    }
+}
+
+var createUserSession = function(oauth_method){
+    return function(accessToken, refreshToken, profile, done){
+	nosqldb.sessionTable.getItem({
+	    signInId: getId(oauth_method, profile),
+	    signInType: oauth_method
+	}, function(err, user){
+	    if (err){
+		console.log("Error trying to find " + oauth_method + " user in NOSQL table: " + err);
+	    }
+	    if (!err && user!=null){
+		done(null, user);
+	    }else{
+		nosqldb.sessionTable.putItem({
+		    signInId: getId(oauth_method, profile),
+		    signInType: oauth_method,
+		    oauthToken: accessToken
+		}, function(err){
+		    if (err){
+			console.log("Error saving " + oauth_method + " user to NOSQL db: " + err);
+		    }else{
+			console.log("Done saving " + oauth_method + " user to NOSQL db");
+			done(null, user);
+		    }
+		});
+	    }
+	});
+    }
+}    
+
+var createGoogleUserSession = function(){
+    return function(identifier, profile, done){
+	nosqldb.sessionTable.getItem({
+	    signInId: profile.identifier,
+	    signInType: 'GOOGLE'
+	}, function(err, user){
+	    if (err){
+		console.log("Error trying to find GOOGLE user in NOSQL table: " + err);
+	    }
+	    if (!err && user!=null){
+		done(null, user);
+	    }else{
+		nosqldb.sessionTable.putItem({
+		    signInId: profile.identifier,
+		    signInType: 'GOOGLE',
+		    oauthToken: 'NA'
+		}, function(err){
+		    if (err){
+			console.log("Error saving GOOGLE user to NOSQL db: " + err);
+		    }else{
+			console.log("Done saving GOOGLE user to NOSQL db");
+			done(null, user);
+		    }
+		});
+	    }
+	});
+    }
+}    
+
+
+// passport stratgies for OAuth
+passport.use(new FacebookStrategy({
+    clientID: ids.facebook.clientID,
+    clientSecret: ids.facebook.clientSecret,
+    callbackURL: ids.facebook.callbackURL
+}, createUserSession('FACEBOOK')));
+
+
+passport.use(new GoogleStrategy({
+    returnURL: ids.google.returnURL,
+    realm: ids.google.realm
+}, createGoogleUserSession()));
+
+passport.use(new TwitterStrategy({
+    consumerKey:    ids.twitter.consumerKey,
+    consumerSecret: ids.twitter.consumerSecret,
+    callbackURL:    ids.twitter.callbackURL
+}, createUserSession('TWITTER')));
+
+passport.serializeUser(function(user, done) {
+    console.log('serializeUser: (' + user.signInId + ',' + user.signInType + ')');
+    done(null, {
+	signInId: user.signInId, 
+	signInType: user.signInType
+    });
+});
+
+passport.deserializeUser(function(user, done) {
+    console.log('DEserializeUser: (' + user.signInId + ',' + user.signInType + ')');
+    nosqldb.sessionTable.getItem({
+        signInId: user.signInId,
+        signInType: user.signInType
+    }, function(err, user2){
+	console.log("DONE Deserialization: " + JSON.stringify(user2, null, 4));
+        if (err){
+	    console.log("Error in deserialization: " + err);
+	    done(err, null);
+	}else{
+	    done(null, user2);
+	}
+    })
+});
+
+module.exports.initialize = function(app){
+    app.use(passport.initialize());
+    app.use(passport.session());
+}
+module.exports.auth_facebook = passport.authenticate('facebook');
+module.exports.callback_facebook = passport.authenticate('facebook', { 
+    successRedirect: '/',
+    failureRedirect: '/login'
+});
+
+module.exports.auth_twitter = passport.authenticate('twitter');
+module.exports.callback_twitter = passport.authenticate('twitter', { 
+    successRedirect: '/',
+    failureRedirect: '/login' 
+});
+
+module.exports.auth_google = passport.authenticate('google');
+module.exports.callback_google = passport.authenticate('google', { 
+    successRedirect: '/',
+    failureRedirect: '/login'
+});
+
+
+
--- server.js
+++ server.js
@@ -5,12 +5,8 @@ var async            = require('async')
   , https            = require('https')
   , jade             = require('jade')
   , path             = require('path')
-  , passport         = require('passport')
-  , TwitterStrategy  = require('passport-twitter').Strategy
-  , FacebookStrategy = require('passport-facebook').Strategy
-  , GoogleStrategy   = require('passport-google').Strategy
-  , oauthConfig      = require('./oauth')
   , routes           = require('./routes')
+  , oauth            = require('./oauth')
   , uploads          = require('./uploads')
   , sqldb            = require('./sql_db')
   , nosqldb          = require('./nosql_db')
@@ -29,101 +25,9 @@ app.configure(function() {
     app.use(express.static(__dirname + '/public'));
     app.use(express.cookieParser());
     app.use(express.session({ secret: 'keyboard cat' }));
-    app.use(passport.initialize());
-    app.use(passport.session());
+    oauth.initialize(app);
     app.use(app.router);
     app.set('port', process.env.PORT || process.env.JOINEDAPP_PORT);
-});
-
-// passport stratgies for OAuth
-passport.use(new FacebookStrategy({
-    clientID: oauthConfig.facebook.clientID,
-    clientSecret: oauthConfig.facebook.clientSecret,
-    callbackURL: oauthConfig.facebook.callbackURL
-    //,profileUrl: "  ..... "
-    //,profileFields : ['email', 'first_name', 'last_name']
-}, function(accessToken, refreshToken, profile, done) {
-    nosqldb.sessionTable.getItem({
-	signInId: profile.id,
-	signInType: 'FACEBOOK'
-    }, function(err, user){
-	if (err){
-	    console.log("Error trying to find Facebook user in NOSQL table: " + err);
-	}
-	if (!err && user!=null){
-	    done(null, user);
-	}else{
-	    nosqldb.sessionTable.putItem({
-		signInId: profile.id,
-		signInType: 'FACEBOOK',
-		oauthToken: accessToken
-	    }, function(err){
-		if (err){
-		    console.log("Error saving Facebook user to NOSQL db: " + err);
-		}else{
-		    console.log("Done saving Facebook user to NOSQL db");
-		    done(null, user);
-		}
-	    });
-	}
-    });
-}));
-
-/*					
-    process.nextTick(function () {
-	console.log("FACEBOOK PROFILE      : " + JSON.stringify(profile, null, 4));
-	console.log("FACEBOOK ACCESS TOKEN : " + accessToken);
-	console.log("FACEBOOK REFRESH TOKEN: " + refreshToken); 
-	return done(null, profile);
-    })
-}));
-*/
-
-passport.use(new GoogleStrategy({
-    returnURL: oauthConfig.google.returnURL,
-    realm: oauthConfig.google.realm
-}, function(identifier, profile, done) {
-    process.nextTick(function(){
-	profile.identifier = identifier;
-	console.log("GOOGLE PROFILE: " + JSON.stringify(profile, null, 4));
-	return done(null, profile);
-    })
-}));
-passport.use(new TwitterStrategy({
-    consumerKey:    oauthConfig.twitter.consumerKey,
-    consumerSecret: oauthConfig.twitter.consumerSecret,
-    callbackURL:    oauthConfig.twitter.callbackURL
-}, function(accessToken, refreshToken, profile, done) {
-    process.nextTick(function(){
-	console.log("TWITTER PROFILE      : " + JSON.stringify(profile, null, 4));
-	console.log("TWITTER ACCESS TOKEN : " + accessToken);
-	console.log("TWITTER REFRESH TOKEN: " + refreshToken); 
-	return done(null, profile);
-    })
-}))
-	     
-passport.serializeUser(function(user, done) {
-    console.log('serializeUser: (' + user.signInId + ',' + user.signInType + ')');
-    done(null, {
-	signInId: user.signInId, 
-	signInType: user.signInType
-    });
-});
-
-passport.deserializeUser(function(user, done) {
-    console.log('DEserializeUser: (' + user.signInId + ',' + user.signInType + ')');
-    nosqldb.sessionTable.getItem({
-        signInId: user.signInId,
-        signInType: user.signInType
-    }, function(err, user2){
-	console.log("DONE Deserialization: " + JSON.stringify(user2, null, 4));
-        if (err){
-	    console.log("Error in deserialization: " + err);
-	    done(err, null);
-	}else{
-	    done(null, user2);
-	}
-    })
 });
 
 // websocket listen to the same address and port
@@ -139,27 +43,22 @@ app.get(path.resolve("/" + uploads.config.storage.dir.path + ':filename'), expre
 app.put('/user', express.bodyParser(), routes.update_user);
 app.delete('/user', express.bodyParser(), routes.delete_user);
 // for authentication
-app.get('/auth/facebook', passport.authenticate('facebook'), function(req, res){
+app.get('/auth/facebook', oauth.auth_facebook, function(req, res){
 });
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { 
-    successRedirect: '/',
-    failureRedirect: '/login'
-}), function(req, res) {
+app.get('/auth/facebook/callback', oauth.callback_facebook, function(req, res) {
     console.log("====================== REDIRECTING to /account FROM FACEBOOK");
     res.redirect('/account');
 });
-app.get('/auth/twitter', passport.authenticate('twitter'), function(req, res){
+app.get('/auth/twitter', oauth.auth_twitter, function(req, res){
 });
-app.get('/auth/twitter/callback', passport.authenticate('twitter', { 
-    successRedirect: '/',
-    failureRedirect: '/login' 
-}));
-app.get('/auth/google', passport.authenticate('google'), function(req, res){
+app.get('/auth/twitter/callback', oauth.callback_twitter, function(req, res){
+    console.log("======================== REDIRECTING TO /account FROM TWITTER");
+    res.redirect('/account');
 });
-app.get('/auth/google/callback', passport.authenticate('google', { 
-    successRedirect: '/',
-    failureRedirect: '/loging'
-}), function(req, res) {
+app.get('/auth/google', oauth.auth_google, function(req, res){
+});
+app.get('/auth/google/callback', oauth.callback_google, function(req, res) {
+    console.log("======================== REDIRECTING TO /account FROM GOOGLE");
     res.redirect('/account');
 });
 app.get('/account', ensureAuthenticated, function(req, res){
@@ -188,59 +87,6 @@ msg.startEventLoop();
 
 // subscribers to pass on to jade template
 app.locals.subscribers = [];
-
-
-/*
-// XXX XXX XXX REMOVE LATER
-nosqldb.sessionTable.describeTable(function(err, response){
-    if (err){
-	console.log("Error in describing NOSQL table: " + err);
-    }else{
-	console.log("NOSQL Table Schema: " + JSON.stringify(response, null, 4));
-    }
-});
-
-nosqldb.sessionTable.putItem({
-    signInId: 'arash@isl.stanford.edu',
-    signInType: 'EMAIL',
-    oauthToken: 'efwuhbciuvb58tbr4vybr8tgyvb8rtgyvb'
-}, function(err, response){
-    if (err){
-	throw(err);
-    }else{
-	console.log("Successfully added item in NOSQL database: " + JSON.stringify(response));
-    }
-});
-
-nosqldb.sessionTable.putItem({
-    signInId: 'arash.hassibi@gmail.com',
-    signInType: 'EMAIL',
-    oauthToken: 'efwuhbciuvb58tbr4vybr8tgyvb8rtgyvb'
-}, function(err, response){
-    if (err){
-	throw(err);
-    }else{
-	console.log("Successfully added item in NOSQL database: " + JSON.stringify(response));
-    }
-});
-
-nosqldb.sessionTable.deleteItem({
-    signInId: 'arash@isl.stanford.edu',
-    signInType: 'EMAIL'
-}, function(err, response){
-    if (err){
-	throw(err);
-    }else{
-	console.log("Successfully deleted item in NOSQL database: " + JSON.stringify(response));
-    }
-});
-*/
-
-
-
-
-
-
 
 // sync the database 
 console.log("Syncing relational database...");
